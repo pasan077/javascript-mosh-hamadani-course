1e7f617540ecea07821129fbd2623b5e63bb55a2
Pramuka Jayathilaka <opasan4all@gmail.com>
2020-07-24 14:07:43 +0530

    24-07-2020
    24-07-2020

--- index.js
+++ index.js
@@ -84,3 +84,37 @@ let employee = {
 };
 
 console.log(employee.getWage());
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+};
+}
+
+const circle1 = createCircle(1);
+circle.draw();
+
+// Constructor Function
+
+function Circle2(radius){
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const another = new Circle2(1);
